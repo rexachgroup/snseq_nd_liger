df4fa4a9d7ec80689a5364752b5425f23117082b
Lawrence Chen <lochen@mednet.ucla.edu>
2020-10-13 14:55:14 -0700

    add individual variable tests to microglia

--- code/clinical_dx_lm_microglia.R
+++ code/clinical_dx_lm_microglia.R
@@ -8,10 +8,12 @@ require(broom)
 require(future.apply)
 require(ggpubr)
 require(patchwork)
+require(nestedRanksTest)
 source("function_library.R")
 source("ggplot_theme.R")
 source("seurat_function_library.R")
 plan(multicore)
+options(future.globals.maxSize = Inf)
 
 in_seurat_expr <- "../analysis/seurat/20200613/tables/seurat_FTD_control_preCG_microglia_expression_matrix.rdat"
 in_seurat_meta <- "../analysis/seurat/20200613/tables/seurat_FTD_control_preCG_microglia_metadata.csv"
@@ -59,63 +61,88 @@ main <- function() {
     cell_type <- "microglia"
     region <- "preCG"
     
-    model_design_set3 <- "expression ~ clinical_dx + sex + finalsite + pmi_h + age + rin + percent_mito + prep"
-    lm_broom_set3 <- run_de_with_lm_by_clinical_dx_and_cell_type(
-        seurat_obj = so,
-        clinical_dx = clinical_dx,
-        region = region,
-        cell_type = cell_type,
-        model_design = model_design_set3,
-        down_sample_cells = 10000)
-    lm_tb_set3 <- filter_and_format_lm_output(
-        lm_out_obj_l = lm_broom_set3, 
-        seurat_obj = so,
-        clinical_dx = clinical_dx,
-        region = region,
-        cell_type = cell_type,
-        model_design = model_design_set3,
-        beta_regex = "clinical_dx")
-
-    precg_micro_bvftd <- so@meta.data %>%
+    cell_id_subset <- so@meta.data %>%
         filter(region == {{region}},
                clinical_dx %in% {{clinical_dx}},
                cluster_cell_type_and_layer == {{cell_type}}) %>%
+        rownames
+    mc_so <- subset(so, cells = cell_id_subset)
+    
+    # Full model.
+    model_design <- "expression ~ clinical_dx + sex + finalsite + pmi_h + age + rin + percent_mito + prep"
+    lm_broom <- run_lm_de(
+        seurat_obj = mc_so,
+        model_design = model_design,
+        down_sample_cells = 10000)
+    lm_tb <- filter_and_format_lm_output(
+        lm_out_obj_l = lm_broom, 
+        seurat_obj = mc_so,
+        model_design = model_design,
+        beta_regex = "clinical_dx")
+    
+
+    # per-variable models.
+    test_vars <- c("sex", "finalsite", "pmi_h", "age", "rin", "percent_mito", "prep")
+    individual_vars <- map(test_vars, function(test_var) {
+        map(clinical_dx, function(dx) {
+            writeLines(str_glue("========== {test_var}, {dx} =========="))
+            cell_id_subset <- mc_so@meta.data %>%
+                filter(clinical_dx == {{dx}}) %>%
+                rownames
+            test_so <- subset(mc_so, cells = cell_id_subset)
+            model <- str_glue("expression ~ {test_var}")
+            i_lm_broom <- run_lm_de(test_so, model, down_sample_cells = 10000)
+            i_lm_tb <- filter_and_format_lm_output(i_lm_broom, test_so, model, beta_regex = str_glue("^{test_var}"))
+
+            i_lm_tb$model <- model
+            i_lm_tb$dx <- dx
+
+            i_lm_tb <- select(i_lm_tb, model, dx, everything())
+            return(i_lm_tb)
+        })
+    })
+
+    precg_micro_bvftd <- mc_so@meta.data %>%
         as_tibble(rownames = "id")
 
-    test_tb <- wilcox_test_cols(precg_micro_bvftd, "clinical_dx", 
+    test_wilcox_tb <- wilcox_test_cols(precg_micro_bvftd, "clinical_dx", 
         list("age", "rin", "percent_mito", "pmi_h", "number_umi", "number_genes", "finalsite", "sex", "prep"))
+    
+    #     test_nestedrank_tb <- nestedrank_test_cols(precg_micro_bvftd, "clinical_dx", "library_id", 
+    #         list("age", "rin", "percent_mito", "pmi_h", "number_umi", "number_genes", "finalsite", "sex", "prep"))
 
-    meta_barplots <- ggplot_grouped_barplot(precg_micro_bvftd, "clinical_dx",
+    meta_barplots <- ggplot_grouped_boxplot(precg_micro_bvftd, "clinical_dx",
         c("age", "rin", "percent_mito", "pmi_h", "number_umi", "number_genes", "prep"))
+   
+    # dump metadata 
     write_csv(precg_micro_bvftd, paste0(out_table, "meta.csv"))
-    write_csv(lm_tb_set3, paste0(out_table, "lm_tb_set3.csv"))
-    write_csv(test_tb, paste0(out_table, "mann-whitney_metadata.csv"))
 
+    # dump full model
+    write_csv(lm_tb, paste0(out_table, "lm_tb_set3.csv"))
+    write_csv(test_wilcox_tb, paste0(out_table, "mann-whitney_metadata.csv"))
+    
+    # dump per-variable models
+    walk(flatten(individual_vars), function(tb) {
+        dx <- unique(tb$dx)
+        term <- unique(tb$term)
+        out_path <- str_glue("{out_table}_individual_{dx}_{term}.csv")
+        writeLines(out_path)
+        write_csv(tb, out_path)
+    })
+    
+    # write metadata barplots
     pdf(paste0(out_graph, "meta_barplots.pdf"), width = 16, height = 8 * (length(meta_barplots) / 2))
     tryCatch({wrap_plots(meta_barplots, ncol = 2)}, error = print)
     dev.off()
 }
 
 
-run_de_with_lm_by_clinical_dx_and_cell_type <- function(
+run_lm_de <- function(
     seurat_obj,
-    clinical_dx,
-    cell_type,
-    region,
     model_design,
     down_sample_cells = NULL,
     cores = NULL){
-
-    # Filter metadata to get cell ids for matching region, dx, cluster_cell_type.
-    # Subset seurat object.
-    cell_id_subset <- seurat_obj@meta.data %>%
-        filter(region == {{region}},
-               clinical_dx %in% {{clinical_dx}},
-               cluster_cell_type_and_layer == {{cell_type}}) %>%
-        rownames
-
-    so <- subset(seurat_obj, cells = cell_id_subset)
-    
+ 
     # Downsample if down_sample_cells defined.
     if (!is.null(down_sample_cells) && ncol(so) > down_sample_cells) {
         writeLines(str_glue("downsampling to {down_sample_cells}"))
@@ -147,7 +174,7 @@ run_lm_broom <- function(expr_m, test_vars, model, cores = NULL) {
         tryCatch({
             broom::tidy(lm(model, data = dat))
         },
-        error = function(e){NA})
+        error = print)
     })
     return(lm_out_obj_l)
 }
@@ -155,52 +182,48 @@ run_lm_broom <- function(expr_m, test_vars, model, cores = NULL) {
 filter_and_format_lm_output <- function(
     lm_out_obj_l, 
     seurat_obj,
-    clinical_dx,
-    cell_type,
-    region,
     model_design,
     beta_regex,
     percent_detected_filter = 0){
 
   print("filter_and_format_lm_output")
 
+  clinical_dx <- unique(seurat_obj[["clinical_dx"]])
+  region <- unique(seurat_obj[["region"]])
+  cell_type <- unique(seurat_obj[["cell_type"]])
   # format lm output into tibble
   lm_out_tb <- bind_rows(lm_out_obj_l, .id = "gene") %>%
     filter(grepl(beta_regex, term) | is.na(term))
 
-  # calculate percent of cells expressing gene
-  # subset seurat object to clinical_dx and cell type of interest
-    cell_id_subset <- seurat_obj@meta.data %>%
-        filter(region == {{region}},
-               clinical_dx %in% {{clinical_dx}},
-               cluster_cell_type_and_layer == {{cell_type}}) %>%
-        rownames
-    seurat_obj <- subset(seurat_obj, cells = cell_id_subset)
+  if (nrow(lm_out_tb) == 0)
+      stop("lm output is of length zero. Check lm_out_obj_l and beta_regex arguments")
+
   expr_m <- GetAssayData(seurat_obj, slot = "data")
   is_ctrl <- seurat_obj@meta.data[["clinical_dx"]] == "Control"
   is_dx <- seurat_obj@meta.data[["clinical_dx"]] != "Control"
   expr_in_lmtb <- expr_m[lm_out_tb$gene, ]
 
   # percent of control cells expressing gene
-  lm_out_tb <- future_apply(expr_in_lmtb, 1, function(row){
+  percent_detected_ctrl <- future_apply(expr_in_lmtb, 1, function(row){
       row_ctrl <- row[is_ctrl]
       percent_detected <- (sum(row_ctrl > 0) / length(row_ctrl)) * 100
-      is.na(percent_detected) <- 0
+      percent_detected[!is.finite(percent_detected)] <- 0
       return(percent_detected)
     }) %>%
     round(1) %>%
-    enframe(name = "gene", value = "percent_detected_ctrl") %>%
-    right_join(., lm_out_tb)
+    enframe(name = "gene", value = "percent_detected_ctrl")
   # percent of dx cells expressing gene
-  lm_out_tb <- future_apply(expr_in_lmtb, 1, function(row){
+  percent_detected_dx <- future_apply(expr_in_lmtb, 1, function(row){
       row_dx <- row[is_dx]
       percent_detected <- (sum(row_dx > 0) / length(row_dx)) * 100
-      is.na(percent_detected) <- 0
+      percent_detected[!is.finite(percent_detected)] <- 0
       return(percent_detected)
     }) %>%
     round(1) %>%
-    enframe(name = "gene", value = "percent_detected_dx") %>%
-    right_join(., lm_out_tb)
+    enframe(name = "gene", value = "percent_detected_dx")
+
+    lm_out_tb <- left_join(lm_out_tb, percent_detected_ctrl, by = "gene") %>%
+        left_join(percent_detected_dx, by = "gene")
 
   # filter to genes expressed in greater than X percent of cells
   print(paste0("number of genes before percent detected filter: ", nrow(lm_out_tb)))
@@ -235,14 +258,7 @@ filter_and_format_lm_output <- function(
     # order by log2 fold change
     arrange(desc(estimate)) %>%
     # convert factors to characters
-    mutate_if(is.factor, as.character) %>%
-    as_tibble()
   # add additional columns
-  lm_out_tb <- lm_out_tb %>% mutate(
-    clinical_dx = paste0(clinical_dx, collapse = "_"),
-    region = region,
-    cell_type = cell_type,
-    model_design = model_design)
 
   print("end of filter_and_format_lm_output")
 
@@ -259,6 +275,24 @@ wilcox_test_cols <- function(tb, group_name, test_list) {
         }
         group_vec <- tb[[group_name]]
         out_tb <- broom::tidy(pairwise.wilcox.test(test_vec, group_vec, paired = FALSE))
+        out_tb$var <- var_name
+        return(out_tb)
+    }) %>% bind_rows()
+}
+
+nestedrank_test_cols <- function(tb, treatment_name, group_name, test_list) {
+    lapply(test_list, function(var_name) {
+        treatment_vec <- tb[[treatment_name]] %>%
+            as.factor() %>% droplevels
+        test_vec <- tb[[var_name]]
+        if (!is.numeric(test_vec)) {
+            test_vec <- as.numeric(factor(test_vec))
+            var_name <- paste0("as_numeric_", var_name)
+        }
+        group_vec <- tb[[group_name]]
+
+        out_tb <- broom::tidy(
+            nestedRanksTest(x = treatment_vec, y = test_vec, group = group_vec))
         out_tb$var <- var_name
         return(out_tb)
     }) %>% bind_rows()
