1c3d7b5d028af1161fe7e688cd1af92573b2eed3
Lawrence Chen <lochen@mednet.ucla.edu>
2020-12-22 11:04:10 -0800

    clinical_dx_lme_microglia: add prop_detected_dx filtering for dge results

--- code/clinical_dx_lme_microglia.R
+++ code/clinical_dx_lme_microglia.R
@@ -26,7 +26,6 @@ print(out_table)
 main <- function() {
     nd_so <- readRDS(in_seurat_rds)
 
-    #     clinical_dx <- c("bvFTD", "Control")
     cell_type <- "microglia"
     region <- "preCG"
 
@@ -37,36 +36,50 @@ main <- function() {
                region == {{region}}) %>%
         pluck("cell_ids")
     mc_so <- subset(nd_so, cells = cell_id_subset)
-    mc_so <- nd_so[, cell_id_subset]
     meta <- mc_so@meta.data
     rm(nd_so)
     gc()
     mc_so[["log_number_umi"]] <- log(mc_so[["number_umi"]])
-    
+
     # Run model.
-    model_design <- "expression ~ clinical_dx + pmi + age + rin + sex + seq_batch + number_umi + percent_mito + Reads.Mapped.Antisense.to.Gene + Fraction.Reads.in.Cells + (1 | library_id)"
     plan(multicore, workers = 12)
-    lm_broom <- run_lmer_de(mc_so, model_design, down_sample_cells = 10000)
-    print(sum(is.na(lm_broom)))
+    model_design <- "expression ~ clinical_dx + pmi + age + rin + sex + seq_batch + number_umi + percent_mito + Reads.Mapped.Antisense.to.Gene + Fraction.Reads.in.Cells + (1 | library_id)"
+    system.time(lm_broom <- run_lmer_de(mc_so, model_design, down_sample_cells = 10000))
+    print(sum(!is.na(lm_broom)))
+
+    # Format output + filter by percent genes detected.
     plan(multicore)
-    lm_tb <- filter_and_format_lm_output(lm_broom[!is.na(lm_broom)], mc_so, model_design, beta_regex = "clinical_dx")
+    system.time({
+        prop_detected <- prop_detected_generate(
+            GetAssayData(mc_so, slot = "data"),
+            mc_so@meta.data
+        )
+    })
+
+    lm_broom_nna <- lm_broom[!is.na(lm_broom)]
+    system.time({
+        lm_tb_0 <- filter_and_format_lm_output(lm_broom_nna, mc_so, model_design,
+            beta_regex = "clinical_dx", prop_detected, prop_detected_filter = 0)
+    })
+    system.time({
+        lm_tb_5 <- filter_and_format_lm_output(lm_broom_nna, mc_so, model_design,
+            beta_regex = "clinical_dx", prop_detected, prop_detected_filter = 0.05)})
+    system.time({
+        lm_tb_10 <- filter_and_format_lm_output(lm_broom_nna, mc_so, model_design,
+            beta_regex = "clinical_dx", prop_detected, prop_detected_filter = 0.10)
+    })
     plan(sequential)
 
 
     # Write out lm_broom. Format and write out lm_tb.
     saveRDS(lm_broom, paste0(out_table, "lm.rds"))
-    saveRDS(lm_tb, paste0(out_table, "lm_tb.rds"))
-    write_csv(lm_tb, paste0(out_table, "lm_tb.csv"))
-    
-    lm_wider_spec <- build_wider_spec(lm_tb,
-        names_from = "term",
-        values_from = -c("gene", "model", "percent_detected_ctrl", "percent_detected_dx", "term"),
-        names_glue = "{term}.{.value}"
-    ) %>% arrange(term)
+    saveRDS(lm_tb_0, paste0(out_table, "lm_0.rds"))
+    saveRDS(lm_tb_5, paste0(out_table, "lm_5.rds"))
+    saveRDS(lm_tb_10, paste0(out_table, "lm_10.rds"))
 
-    lm_tb %>% 
-        pivot_wider_spec(lm_wider_spec) %>%
-        write_csv(., paste0(out_table, "lm_wider_tb.csv"))    
+    write_csv(lm_tb_0, paste0(out_table, "lm_wider_tb_0.csv"))
+    write_csv(lm_tb_5, paste0(out_table, "lm_wider_tb_5.csv"))
+    write_csv(lm_tb_10, paste0(out_table, "lm_wider_tb_10.csv"))
 }
 
 
@@ -75,11 +88,11 @@ run_lmer_de <- function(
     model_design,
     down_sample_cells = NULL,
     cores = NULL){
- 
+
     # Downsample if down_sample_cells defined.
     if (!is.null(down_sample_cells) && ncol(seurat_obj) > down_sample_cells) {
         writeLines(str_glue("downsampling to {down_sample_cells}"))
-        seurat_obj <- subset(seurat_obj, cells = sample(colnames(seurat_obj), down_sample_cells))
+       seurat_obj <- subset(seurat_obj, cells = sample(colnames(seurat_obj), down_sample_cells))
         seurat_obj@meta.data %>%
             select(region, clinical_dx, cluster_cell_type) %>% table %>% print
     }
@@ -94,7 +107,7 @@ run_lmer_de <- function(
     if ("clinical_dx" %in% colnames(test_vars)) {
         test_vars <- mutate(test_vars, clinical_dx = fct_relevel(clinical_dx, "Control"))
     }
-    
+
     run_lmer_broom(expr_m, test_vars, model)
 }
 
@@ -111,88 +124,103 @@ run_lmer_broom <- function(expr_m, test_vars, model, cores = NULL) {
 }
 
 
+prop_cells_detected_dx <- function(expr, meta, clinical_dx_val) {
+    is_dx <- meta[["clinical_dx"]] == clinical_dx_val
+    prop_detected_dx <- future_apply(expr, 1, function(row) {
+        row_dx <- row[is_dx]
+        prop_detected <- (sum(row_dx > 0) / length(row_dx))
+        prop_detected[!is.finite(prop_detected)] <- 0
+        return(prop_detected)
+    }) %>%
+    round(3) %>%
+    enframe(name = "gene", value = paste0("prop_detected_", clinical_dx_val))
+    return(prop_detected_dx)
+}
+
+prop_detected_generate <- function(expr, meta) {
+    # proportion of contrl cells expressing gene
+    ctl_detect <- prop_cells_detected_dx(expr, meta, "Control")
+
+    # proportion of dx cells expressing gene
+    ad_detect <- prop_cells_detected_dx(expr, meta, "AD")
+    ftd_detect <- prop_cells_detected_dx(expr, meta, "bvFTD")
+    psp_detect <- prop_cells_detected_dx(expr, meta, "PSP-S")
+
+    return(reduce(list(ad_detect, ftd_detect, psp_detect),
+           inner_join,
+           by = "gene",
+           .init = ctl_detect))
+}
+
 filter_and_format_lm_output <- function(
-    lm_out_obj_l, 
+    lm_out_obj_l,
     seurat_obj,
     model_design,
     beta_regex,
-    percent_detected_filter = 0){
+    prop_detected,
+    prop_detected_filter = 0){
 
-    print("filter_and_format_lm_output")
-
-    clinical_dx <- unique(seurat_obj[["clinical_dx"]])
-    region <- unique(seurat_obj[["region"]])
-    cell_type <- unique(seurat_obj[["cell_type"]])
     # format lm output into tibble
-    lm_out_tb <- bind_rows(lm_out_obj_l, .id = "gene") %>%
-    filter(grepl(beta_regex, term) | is.na(term))
+    lm_tb <- lm_out_obj_l %>%
+        bind_rows(.id = "gene") %>%
+        filter(grepl(beta_regex, term) | is.na(term))
 
-    if (nrow(lm_out_tb) == 0)
+    # pivot dx output.
+    lm_wider_spec <- build_wider_spec(lm_tb,
+        names_from = "term",
+        values_from = c("estimate", "p.value", "std.error", "statistic", "df"),
+        names_glue = "{term}.{.value}"
+    ) %>% arrange(term)
+    lm_wd_tb <- pivot_wider_spec(lm_tb, id_cols = "gene", lm_wider_spec)
+
+    if (nrow(lm_wd_tb) == 0)
         stop("lm output is of length zero. Check lm_out_obj_l and beta_regex arguments")
 
-    expr_m <- GetAssayData(seurat_obj, slot = "data")
-    is_ctrl <- seurat_obj@meta.data[["clinical_dx"]] == "Control"
-    is_dx <- seurat_obj@meta.data[["clinical_dx"]] != "Control"
-    expr_in_lmtb <- expr_m[unique(lm_out_tb$gene), ]
+    meta <- seurat_obj@meta.data
 
-    # percent of control cells expressing gene
-    percent_detected_ctrl <- future_apply(expr_in_lmtb, 1, function(row){
-        row_ctrl <- row[is_ctrl]
-        percent_detected <- (sum(row_ctrl > 0) / length(row_ctrl)) * 100
-        percent_detected[!is.finite(percent_detected)] <- 0
-        return(percent_detected)
-    }) %>%
-    round(1) %>%
-    enframe(name = "gene", value = "percent_detected_ctrl")
 
-    # percent of dx cells expressing gene
-    percent_detected_dx <- future_apply(expr_in_lmtb, 1, function(row){
-        row_dx <- row[is_dx]
-        percent_detected <- (sum(row_dx > 0) / length(row_dx)) * 100
-        percent_detected[!is.finite(percent_detected)] <- 0
-        return(percent_detected)
-    }) %>%
-    round(1) %>%
-    enframe(name = "gene", value = "percent_detected_dx")
+    # join prop_detected with lm_wd_tb
+    lm_out_prop <- inner_join(lm_wd_tb, prop_detected, by = "gene")
 
-    lm_out_tb <- left_join(lm_out_tb, percent_detected_ctrl, by = "gene") %>%
-        left_join(percent_detected_dx, by = "gene")
-
-    # filter to genes expressed in greater than X percent of cells
-    print(paste0("number of genes before percent detected filter: ", nrow(lm_out_tb)))
-    lm_out_tb <- lm_out_tb %>%
-        filter(percent_detected_dx > percent_detected_filter | percent_detected_ctrl > percent_detected_filter)
-    print(paste0("number of genes after percent detected filter: ", nrow(lm_out_tb)))
+    # filter to genes expressed in greater than X prop of cells
+    print(paste0("number of genes before prop detected filter: ", nrow(lm_out_prop)))
+    lm_filter_tb <- lm_out_prop %>%
+        filter(prop_detected_AD > prop_detected_filter |
+               prop_detected_Control > prop_detected_filter |
+               prop_detected_bvFTD > prop_detected_filter |
+               `prop_detected_PSP-S` > prop_detected_filter )
+    print(paste0("number of genes after prop detected filter: ", nrow(lm_filter_tb)))
 
     # fdr_pvalue correct
-    lm_out_tb$fdr_pvalue <- p.adjust(lm_out_tb$p.value, method = "BH")
-    # check
-    table(lm_out_tb$p.value < 0.05) %>% print()
-    table(lm_out_tb$fdr_pvalue < 0.05) %>% print()
+    lm_filter_fdr <- lm_filter_tb %>%
+        mutate(
+            clinical_dxAD.fdr.p.value = p.adjust(clinical_dxAD.p.value, method = "BH"),
+            clinical_dxbvFTD.fdr.p.value = p.adjust(clinical_dxbvFTD.p.value, method = "BH"),
+            `clinical_dxPSP-S.fdr.p.value` = p.adjust(`clinical_dxPSP-S.p.value`, method = "BH")
+        )
+
+    # print pval counts after correction
+    lm_filter_fdr %>%
+        summarize(across(contains("p.value"), ~sum(.x < 0.05))) %>%
+        print(width = Inf)
 
     # formating
-    lm_out_tb <- lm_out_tb %>%
-    # order columns
-    dplyr::select(
-      gene,
-      term = term,
-      estimate = estimate,
-      t_statistic = statistic,
-      percent_detected_dx,
-      percent_detected_ctrl,
-      pvalue = p.value,
-      fdr_pvalue,
-      everything()) %>%
-    # order by log2 fold change
-    arrange(desc(estimate))
-    # convert factors to characters
-    # add additional columns
-    lm_out_tb[["model"]] <- model_design
+    lm_filter_out <- lm_filter_fdr %>%
+        dplyr::select(
+            gene,
+            contains("estimate"),
+            contains("p.value"),
+            contains("statistic"),
+            contains("prop_detected"),
+            contains("fdr.p.value")
+        ) %>%
+        mutate(
+            model = model_design,
+            region = paste0(unique(meta$region)),
+            cell_type = paste0(unique(meta$cell_type))
+        )
 
-    print("end of filter_and_format_lm_output")
-
-    return(lm_out_tb)
-
+    return(lm_filter_out)
 }
 
 if (interactive()) {
