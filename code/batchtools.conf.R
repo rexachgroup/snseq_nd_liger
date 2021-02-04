381ab58e80da8399893f8d98c2f358dfb6493493
Lawrence Chen <lochen@mednet.ucla.edu>
2021-02-04 15:20:40 -0800

    track batchtools

---
+++ code/batchtools.conf.R
@@ -0,0 +1,11 @@
+library(rprojroot)
+root <- rprojroot::has_file(".git/index")
+ORION_SLURM_TMPL = root$find_file("code/slurm.tmpl")
+#HOFFMAN_SGE_TMPL = "sge.tmpl"
+
+is_orion <- function() dir.exists("/geschwindlabshares")
+
+if (is_orion()) {
+    compress = FALSE
+    cluster.functions <- makeClusterFunctionsSlurm(template = ORION_SLURM_TMPL)
+}
