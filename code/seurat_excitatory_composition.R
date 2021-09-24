b805d2bf8cf072173c22e50b4bba73e0c82e2ad3
Lawrence Chen <lochen@mednet.ucla.edu>
2021-09-24 09:52:00 -0700

    seurat_excitatory_composition: generate pvalue brackets by rstatix::hsd_tukey
    Former-commit-id: 93a9b6a860e08cdcb6ac1c68fbf7efb12a03142c

--- code/seurat_excitatory_composition.R
+++ code/seurat_excitatory_composition.R
@@ -1,6 +1,6 @@
 # Per-dx celltype proportions for all control samples.
 # Downsampling per dx / region to get consistent propoertion estimates(?) between different regions.
-liblist <- c("tidyverse", "Seurat", "ggpubr", "broom")
+liblist <- c("tidyverse", "Seurat", "ggpubr", "broom", "rstatix")
 lapply(liblist, require, character.only = TRUE, quiet = TRUE)
 commit_id <- str_glue("{system('git show --format=\"%h\" -s', intern = TRUE)}")
 
@@ -30,32 +30,51 @@ main <- function() {
         filter(!cluster_celltype_layer %in% c("pericyte", "ependymal", "t_cell")) %>%
         ungroup %>%
         mutate(clinical_dx = fct_relevel(clinical_dx, "Control")) %>%
-        group_by(library_id, clinical_dx, region, cluster_celltype_layer) %>%
-        summarise(n = n(), .groups = "drop") %>%
+        group_by(library_id, autopsy_id, clinical_dx, region, cluster_celltype_layer) %>%
+        summarise(n = n(), percent_mito = mean(percent_mito), age = first(age), pmi = first(pmi), sex = first(sex), .groups = "drop") %>%
         group_by(library_id) %>%
         mutate(percent_of_library = n / sum(n)) %>%
         arrange(region, clinical_dx, cluster_celltype_layer)
+    
+    meta_lm <- cluster_layer_dat %>%
+        group_by(cluster_celltype_layer, region) %>%
+        summarize(lm_matrix = list(tidy(lm(percent_of_library ~ clinical_dx + sex + age + pmi + percent_mito), data = .))) %>%
+        unnest(lm_matrix) %>%
+        filter(grepl("clinical_dx", term))
+
+    meta_lm$group1 = str_replace(meta_lm$term, "clinical_dx", "")
+    meta_lm$group2 = "Control"
+    meta_lm$y.position = "1"
+
+    meta_lm <- cluster_layer_dat %>%
+        group_by(cluster_celltype_layer, region) %>%
+        tukey_hsd(percent_of_library ~ clinical_dx) %>%
+        add_xy_position(x = "clinical_dx", fun = "mean_se")
 
     write_csv(layer_dat, file.path(out_dir, "celltype_layer_proportions.csv"))
     write_csv(cluster_layer_dat, file.path(out_dir, "cluster_celltype_layer_proportions.csv"))
 
     pdf(file.path(out_dir, "cluster_celltype_layer_proprtion_dx_test.pdf"), width = 3 * length(unique(cluster_layer_dat$cluster_celltype_layer)), height = 2 * length(unique(cluster_layer_dat$region)))
 
-    my_comparisons <- list(c(1, 2), c(1, 3), c(1, 4))
-    plt <- ggplot(cluster_layer_dat, aes(x = clinical_dx, y = percent_of_library, fill = clinical_dx)) +
-        geom_boxplot(outlier.shape = NA) +
-        facet_grid("region ~ cluster_celltype_layer") +
-        geom_jitter(color = "black", alpha = 0.5, position = position_jitter(seed = 1)) +
-        stat_compare_means(
-            aes(group = clinical_dx, label = round(as_label(..p.format..), 2)),
-            comparisons = my_comparisons,
-            method = "wilcox.test",
-            p.adjust.method = "none", 
-            label.y.npc = 0
+  #  my_comparisons <- list(c(1, 2), c(1, 3), c(1, 4))
+    #     plt1 <- ggplot(cluster_layer_dat, aes_string(x = "clinical_dx", y = "percent_of_library", color = "clinical_dx")) +
+    #         facet_grid(c("region", "cluster_celltype_layer"), scales = "free") +
+    #         geom_boxplot() +
+    #         geom_jitter(color = "black", alpha = 0.5, position = position_jitter(seed = 1)) +
+    #         stat_pvalue_manual(
+    #             meta_lm,
+    #             label = "p.value"
+    #         ) +
+    #         coord_flip() +
+    #         ggtitle(str_glue("{date()} {commit_id} cell type percentage of library + significance testing between dx groupings"))
+    plt2 <- ggboxplot(cluster_layer_dat, x = "clinical_dx", y = "percent_of_library", color = "clinical_dx", facet = c("region", "cluster_celltype_layer")) +
+        stat_pvalue_manual(
+            meta_lm
         ) +
         coord_flip() +
         ggtitle(str_glue("{date()} {commit_id} cell type percentage of library + significance testing between dx groupings"))
-    print(plt)
+    #     print(plt1)
+    print(plt2)
     graphics.off()
 }
 
