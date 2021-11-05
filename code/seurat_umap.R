9a9465ff47ebed0fb2b582b915f700042cd7c683
Lawrence Chen <lochen@mednet.ucla.edu>
2021-11-05 11:39:23 -0700

    seurat_umap: add umap by celltype
    Former-commit-id: 40ed8250ec40ab953d331ae2f49570c8128299ab

--- code/seurat_umap.R
+++ code/seurat_umap.R
@@ -13,7 +13,7 @@ nk_markers <- "../resources/Immune_markers_toplot_2021.xlsx"
 sobj_umap_path <- file.path(out_dir, "sobj_meta_umap.rds")
 sobj_umap_expr_subset_path <- file.path(out_dir, "sobj_expr_subset.rds")
 
-plot_ts <- str_glue("seurat_umap.R {date()} {system('git show --format=\"%h\" -s', intern = TRUE)}")
+plot_ts <- str_glue("{date()} {system('md5sum seurat_umap.R', intern = TRUE)}")
 pal_stallion = c("1"="#D51F26","2"="#272E6A","3"="#208A42","4"="#89288F","5"="#F47D2B", "6"="#FEE500","7"="#8A9FD1","8"="#C06CAB","19"="#E6C2DC",
                 "10"="#90D5E4", "11"="#89C75F","12"="#F37B7D","13"="#9983BD","14"="#D24B27","15"="#3BBCA8", "16"="#6E4B9E","17"="#0C727C", "18"="#7E1416","9"="#D8A767","20"="#3D3D3D")
 
@@ -28,6 +28,7 @@ main <- function() {
     pdf(file.path(out_dir, "sobj_umap.pdf"), width = 10, height = 7)
     print(ggplot_umap_dx(umap_meta))
     print(ggplot_umap_region(umap_meta))
+    print(ggplot_umap_cluster(umap_meta))
     graphics.off()
 
     nk_list <- ggplot_umap_expr_mat(umap_meta, nk_expr_mat)
@@ -73,6 +74,18 @@ ggplot_umap_region <- function(meta) {
         ggtitle(str_glue("{plot_ts} region umap"))
 }
 
+ggplot_umap_cluster <- function(meta) {
+    cluster_centers <- meta %>%
+        group_by(cluster_celltype_layer) %>%
+        summarize(cluster_x = mean(UMAP_1), cluster_y = mean(UMAP_2))
+    ggplot(meta, aes(x = UMAP_1, y = UMAP_2, color = cluster_celltype_layer)) + 
+        rasterize(geom_point(size = 0), dpi = 300, dev = "ragg_png") +
+        geom_text(data = cluster_centers, mapping = aes(x = cluster_x, y = cluster_y, label = cluster_celltype_layer), colour = "black", fill = "black") +
+        scale_color_manual(values = setNames(pal_stallion, NULL)) +
+        theme(aspect.ratio = 1) +
+        ggtitle(str_glue("{plot_ts} cluster umap"))
+}
+
 ggplot_umap_expr_mat <- function(meta, expr_mat) {
     map(rownames(expr_mat), function(gene_name) {
         expr_tb <- expr_mat[gene_name, ] %>%
@@ -85,7 +98,7 @@ ggplot_umap_expr_mat <- function(meta, expr_mat) {
             select(cell_ids, UMAP_1, UMAP_2, gene_expr) %>%
             mutate(alpha = ifelse(gene_expr > 0, 0.8, 0.2))
         gg <- ggplot(plot_tb, aes(x = UMAP_1, y = UMAP_2, color = gene_expr, alpha = alpha)) +
-            rasterize(geom_point(size = 1), dpi = 200, dev = "ragg_png") +
+            rasterize(geom_point(size = 0), dpi = 200, dev = "ragg_png") +
             scale_color_distiller(type = "div", palette = "RdGy", direction = -1, na.value = "grey90") + 
             theme(aspect.ratio = 1) +
             ggtitle(str_glue("{gene_name}"))
