d2b912ced5b4e378918f7933fbe6bbf8498240de
Lawrence Chen <lochen@mednet.ucla.edu>
2022-02-09 09:58:25 -0800

    batchtools: add default nessie runner
    Former-commit-id: d8bd03624ddea1790aa0f9306d145ec92f39e261

--- code/batchtools.conf.R
+++ code/batchtools.conf.R
@@ -4,8 +4,14 @@ ORION_SLURM_TMPL = root$find_file("code/slurm.tmpl")
 #HOFFMAN_SGE_TMPL = "sge.tmpl"
 
 is_orion <- function() dir.exists("/geschwindlabshares")
+is_nessie <- function() system("hostname", intern = TRUE) == "nessie.bmap.ucla.edu"
 
 if (is_orion()) {
     compress = FALSE
     cluster.functions <- makeClusterFunctionsSlurm(template = ORION_SLURM_TMPL)
+}
+
+if (is_nessie()) {
+    compress = FALSE
+    cluster.functions = makeClusterFunctionsMulticore()
 }
