80b48855aade956f3332c4dcf8481da8bc4ead13
Lawrence Chen <lochen@mednet.ucla.edu>
2023-01-30 10:05:53 -0800

    scenic_hier: change sorting, add additional scenic results
    Former-commit-id: 0d0bc9d95ca0e17e3c7105158800825413514c82

--- code/scenic/scenic_hier.R
+++ code/scenic/scenic_hier.R
@@ -1,4 +1,4 @@
-# Hierarchical clusteirng + heatmap of scenic regulons.
+# Hierarchical clustering + heatmap of scenic regulons.
 
 liblist <- c("tidyverse", "ggplot2", "ComplexHeatmap", "circlize", "scales", "patchwork")
 l <- lapply(liblist, library, character.only = TRUE, quietly = TRUE)
@@ -6,6 +6,7 @@ l <- lapply(liblist, library, character.only = TRUE, quietly = TRUE)
 SCENIC_TABLES <- "../../resources/scenic/rss"
 OUT_DIR <- "../../analysis/seurat_lchen/scenic_hier/"
 plot_ts <- function() { str_glue("{date()} {system('md5sum scenic_hier.R', intern = T)}")}
+k_display <- 25
 
 main <- function() {
     dir.create(OUT_DIR)
@@ -13,50 +14,41 @@ main <- function() {
     tables <- tables %>% mutate(
         data = map(path, read_csv),
         basename = str_match(basename(path), "(regulon_specificity_score)(.*)(.csv)")[,3],
-        celltype = str_match(basename, "(.+)_(.+)")[,2],
-        region = str_match(basename, "(.+)_(.+)")[,3],
-        out_path = str_glue("{OUT_DIR}/{basename}_mds_grouped.pdf")
+        celltype = str_match(basename, "_*(.+)_(.+)")[,2],
+        region = str_match(basename, "_*(.+)_(.+)")[,3]
     )
     
     tables <- tables %>%
         mutate(regulon_data = pmap(., function(...) { 
             cr <- list(...)
-            #tb <- cr$data[2:5]# AD_[region], bvFTD_[region], Control_[region], PSP-S_[region]
             tb <- rename(cr$data, regulon = `...1`)
-            tb <- mutate(tb, regulon = str_match(regulon, "^[a-zA-Z0-9]+")[,1]) # only 1st alphanumeric component, drop _exended and (\\d+g)
+            tb <- mutate(tb, regulon = str_match(regulon, "^[a-zA-Z0-9]+")[,1]) # only 1st alphanumeric component, drop _extended and (\\d+g)
+            
+            # subset to rss rank columns and convert to matrix
             ad_col <- str_glue("AD_{cr$celltype}_rank")
             ftd_col <- str_glue("bvFTD_{cr$celltype}_rank") 
             psp_col <- str_glue("PSP-S_{cr$celltype}_rank") 
             control_col <- str_glue("Control_{cr$celltype}_rank")
 
-            tb <- select(tb, regulon, ad_col, ftd_col, psp_col, control_col)
+            # sort by the minimum rank of any dx, then subset to first k_display rows
+            tb <- select(tb, regulon, all_of(c(ad_col, ftd_col, psp_col, control_col)))
+            tb$min <- pmin(tb[[ad_col]], tb[[ftd_col]], tb[[psp_col]], tb[[control_col]])
+            tb <- tb %>% arrange(min) %>% slice_head(n = k_display) %>% select(-min)
 
-            # filter to top 20 per dx.
-            ad_pts <- slice_col_head(tb, ad_col, 20)
-            ftd_pts <- slice_col_head(tb, ftd_col, 20)
-            psp_pts <- slice_col_head(tb, psp_col,20)
-            ctl_pts <- slice_col_head(tb, control_col, 20)
-            all_pts <- bind_rows(ctl_pts, ad_pts, ftd_pts, psp_pts) %>% 
-                filter(!duplicated(regulon))
-
-            all_pts %>%
+            tb_mat <- tb %>%
                 as.data.frame %>% 
                 column_to_rownames("regulon") %>%
                 as.matrix
+
+            return(tb_mat)
         }))
 
-    tables <- tables %>%
-        mutate(regulon_hier = pmap(., function(...) {
-            cr <- list(...)
-            regulon_dist <- dist(cr$regulon_data)            
-            hier <- hclust(regulon_dist)
-            return(hier)
-        }))
-
-    pdf(file.path(OUT_DIR, "heatmap.pdf"), height = 20, width = 10) 
+    pdf(file.path(OUT_DIR, "heatmap.pdf"), height = 10, width = 10) 
     subtitle <- plot_ts()
     pwalk(tables, function(...) {
         cr <- list(...)
+        hier <- hclust(dist(cr$regulon_data))
+
         colormap <- circlize::colorRamp2(
             breaks = quantile(cr$regulon_data, seq(0, 1, by = 0.1)),
             viridisLite::viridis(11, direction = -1)
@@ -64,9 +56,9 @@ main <- function() {
         tmp_pdf <- function(w, h) { pdf(tempfile(), w, h) }
         heat <- Heatmap(
             cr$regulon_data,
-            cluster_rows = cr$regulon_hier,
+            cluster_rows = hier,
             cluster_columns = F,
-            name = "regulon rank",
+            name = "rss rank",
             col = colormap
         )
         heatmap_gtree <- grid.grabExpr(
@@ -76,16 +68,11 @@ main <- function() {
             width = unit(20, "cm"),
             height = unit(20, "cm")
         )
-        title <- str_glue("{cr$basename} regulon rank clustering, top 20 each dx")
+        title <- str_glue("{cr$basename} regulon rss rank clustering, top {k_display} regulon")
         print(wrap_plots(heatmap_gtree) + plot_annotation(title = title, subtitle = subtitle))
 
     })
     graphics.off()
-
-}
-
-slice_col_head <- function(tb, col, thresh) {
-    tb %>% filter(.data[[col]] < thresh)
 }
 
 main()
